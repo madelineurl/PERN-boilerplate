baaa2056854b767e96d6cd9c65eea5913292f647
Maddie Higgins <maddiehiggins9@gmail.com>
2020-11-27 11:11:48 -0800

    remove react

--- client/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-
-console.log('hello from JS!');
-
-ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 module.exports = {
   mode: "development",
-  entry:  ["@babel/polyfill", './client/index.js'],
+  entry:  ["@babel/polyfill", './src/index.js'],
   output: {
-    path: __dirname,
-    filename: './public/bundle.js'
+    path: __dirname + '/public',
+    filename: 'bundle.js'
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
+  // resolve: {
+  //   extensions: ['.js', '.jsx']
+  // },
   devtool: 'source-map',
   watchOptions: {
     ignored: /node_modules/
   },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.jsx?$/,
+  //       exclude: /node_modules/,
+  //       loader: 'babel-loader'
+  //     }
+  //   ]
+  // }
 }
