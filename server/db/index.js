b599d68b59f6d30fb45a7479efb2aa2ca44dd5d9
Madeline Higgins <64429419+madelineurl@users.noreply.github.com>
2021-02-20 16:46:28 -0600

    update default db to dummy (YOUR DB HERE)

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize('postgres://localhost:5432/stratfs', {
+const db = new Sequelize('postgres://localhost:5432/YOUR_DB_HERE', {
   logging: false
 });
 
